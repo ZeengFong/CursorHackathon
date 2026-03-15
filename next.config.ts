0f93b084a77bb4ac05bb46c8b37f15982f77d589
ZeengFong <163157869+ZeengFong@users.noreply.github.com>
2026-03-14 20:01:11 -0600

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    proxyClientMaxBodySize: 50 * 1024 * 1024, // 50 MB
+  },
 };
 
 export default nextConfig;
